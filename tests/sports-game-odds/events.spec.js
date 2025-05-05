6b8f9c16420a642e59332c680f356c17d4e53f27
Bellanov Apilli <bellanovapilli@gmail.com>
2025-05-05 17:31:03 -0400

    Initial Unit Tests

--- tests/sports-game-odds/events.spec.js
+++ tests/sports-game-odds/events.spec.js
@@ -7,12 +7,12 @@ import 'dotenv/config';
 import * as SportsEvents from "../../scripts/extract/sports-game-odds/events.js";
 
 
-describe("A suite", function () {
-  it("contains a spec with an expectation", function () {
-    // Identify sport to query
+describe("SportsEvents", function () {
+  it("Query sport events", function () {
+    // Identify league to query
     const leagueID = "UFC"
 
-    // Query sport Events
+    // Query sports Events
     const events = SportsEvents.getEvents(leagueID);
     expect(events).not.toBe(null);
   });
