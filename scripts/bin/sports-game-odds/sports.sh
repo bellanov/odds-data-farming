119bc588d2b6eb5fc90940b4666ca19111196f61
Bellanov Apilli <bellanovapilli@gmail.com>
2025-05-08 22:01:57 -0400

    Applied Google Style Standards

--- scripts/bin/sports-game-odds/sports.sh
+++ scripts/bin/sports-game-odds/sports.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 #
-# Query Sports Data.
+# Extract Sports Data.
 
+
+#######################################
+# Display error message.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   Writes log message to stderr
+#######################################
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
+#######################################
+# Display log messages.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   Writes log message to stdout
+#######################################
+info() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
+}
+
+
+# Begin script execution
+info "Starting sports data extraction..."
+
 # Execute the script to fetch sports data
 node scripts/extract/sports-game-odds/sports.js
 
+# Check if the script executed successfully
 if [ $? -ne 0 ]; then
   err "Failed to execute sports data extraction script"
   exit 1
 fi
+
+# Log completion message
+info "Sports data extraction completed successfully."
--- scripts/bin/the-odds-api/sports.sh
+++ scripts/bin/the-odds-api/sports.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 #
-# Query Sports Data.
+# Extract Sports Data.
 
+
+#######################################
+# Display error message.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   Writes log message to stderr
+#######################################
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  exit 1
 }
+
+#######################################
+# Display log messages.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   Writes log message to stdout
+#######################################
+info() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
+}
+
+# Begin script execution
+info "Starting sports data extraction..."
 
 # Execute the script to fetch sports data
 node scripts/extract/the-odds-api/sports.js
 
+# Check if the script executed successfully
 if [ $? -ne 0 ]; then
   err "Failed to execute sports data extraction script"
-  exit 1
 fi
+
+# Log completion message
+info "Sports data extraction completed successfully."
